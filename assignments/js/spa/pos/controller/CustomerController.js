9b272255d2ca3ad5990acf57cfb8285373638a8e
AsiriM2000 <asiriambagahawaththa@gmail.com>
2022-10-11 19:08:51 +0530

    content update

--- assignments/js/spa/pos/controller/CustomerController.js
+++ assignments/js/spa/pos/controller/CustomerController.js
@@ -1,6 +1,6 @@
 
 // Button Events
-$("saveCustomer").click(function (){
+$("#saveCustomer").click(function (){
 
     let customerID = $("#txtCustomerID").val();
     let customerName = $("#txtCustomerName").val();
@@ -19,14 +19,25 @@ $("saveCustomer").click(function (){
 
     // push data
     customers.push(customerObject);
+
+    //load all data
+    loadAllCustomers();
 });
 
 function loadAllCustomers() {
     $("#tblCustomer").empty();
 
-    for (var customer of customers){
-        var cusData = `<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.address}</td><td>${customer.contact}</td><td>${customer.salary}</td></tr>`;
+
+    for (var customer of customers) {
+
+        var all = `<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.address}</td><td>${customer.contact}</td><td>${customer.salary}</td><td class="odd" style="width: 190px">
+                            <button class="btn btn-warning btn-mini" data-bs-target="#editCustomers"
+                                    data-bs-toggle="modal"><i class="fa-solid fa-pen-to-square"></i> Edit
+                            </button>
+                            <button class="btn btn-danger btn-mini"><i class="fa-solid fa-trash"></i> Delete</button>
+                        </td></tr>`;
+
+        $("#tblCustomer").append(all);
     }
 
-    $("#tblCustomer").append(cusData);
 }
