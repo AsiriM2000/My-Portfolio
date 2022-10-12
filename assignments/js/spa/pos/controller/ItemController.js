a2f45e4b6bdf324afd437bdb22f6c0e8271402ec
AsiriM2000 <asiriambagahawaththa@gmail.com>
2022-10-12 19:20:38 +0530

    content update

--- assignments/js/spa/pos/controller/ItemController.js
+++ assignments/js/spa/pos/controller/ItemController.js
@@ -4,14 +4,13 @@ $("#saveItem").click(function (){
     let itemName = $("#txtItemName").val();
     let qty = $("#txtItemQty").val();
     let unitPrice = $("#txtItemUnitPrice").val();
-    let total = $("#txtItemTotal").val();
 
     var itemObject = {
         code: itemCode,
         itemName: itemName,
         qty: qty,
         unitPrice: unitPrice,
-        total: total
+
     }
 
     items.push(itemObject);
@@ -23,7 +22,7 @@ function loadAllItems() {
     $("#tblItem").empty();
 
     for (var item of items){
-        var all = `<tr><td>${item.code}</td><td>${item.itemName}</td><td>${item.qty}</td><td>${item.unitPrice}</td><td>${item.total}</td>
+        var all = `<tr><td>${item.code}</td><td>${item.itemName}</td><td>${item.qty}</td><td>${item.unitPrice}</td>
                         <td><button class="btn btn-warning btn-mini" data-bs-target="#editItems"
                         data-bs-toggle="modal" id="btn-edit"><i class="fa-solid fa-pen-to-square"></i> Edit
                         </button>
