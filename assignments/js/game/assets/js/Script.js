abfec93b7efdf7cad3729a20cd6ca99430383980
AsiriM2000 <asiriambagahawaththa@gmail.com>
2022-11-19 17:27:10 +0530

    all changes complited

--- assignments/js/game/assets/js/Script.js
+++ assignments/js/game/assets/js/Script.js
@@ -1,29 +1,28 @@
 let boy = document.getElementById("boy");
+
 idleImageNumber = 0;
 idleAnimationNumber = 0;
+flyImageNumber = 0;
+flyAnimationNumber = 0;
+
+function flyAnimation() {
+    flyImageNumber = flyImageNumber + 1;
+    if (flyImageNumber == 11) {
+        flyImageNumber = 1;
+    }
+    boy.src = "assets/image/test/Idle%20(1)(" + idleImageNumber + ").png";
+}
+
+function flyAnimationStart() {
+    flyAnimationNumber = setInterval(flyAnimation, 100);
+    clearInterval(idleAnimationNumber);
+}
+
+function keyCheck(event){
+    var keyCode = event.which;
 
 
-// flyImageNumber = 0;
-// flyAnimationNumber = 0;
-//
-// function flyAnimation() {
-//     flyImageNumber = flyImageNumber + 1;
-//     if (flyImageNumber == 3) {
-//         flyImageNumber = 1;
-//     }
-//     boy.src = "assets/image/test/Fly%20(" + idleImageNumber + ").png";
-// }
-//
-// function flyAnimationStart() {
-//     flyAnimationNumber = setInterval(flyAnimation, 100);
-//     clearInterval(idleAnimationNumber);
-// }
-//
-// function keyCheck(event){
-//     var keyCode = event.which;
-//
-//
-// }
+}
 
 function keyCheck(event) {
     var keyCode = event.which;
@@ -36,17 +35,16 @@ function keyCheck(event) {
 
     function idleAnimation() {
         idleImageNumber = idleImageNumber + 1;
-        if (idleImageNumber == 3) {
+        if (idleImageNumber == 9) {
             idleImageNumber = 1;
         }
-        boy.src = "assets/image/characters/Fly%20(" + idleImageNumber + ").png";
+        boy.src = "assets/image/characters/Run%20(" + idleImageNumber + ").png";
     }
 
     idleAnimationNumber = setInterval(idleAnimation, 200);
 
 
 }
-
 var backgroundImagePositionX =0;
 var moveBackgroundAnimationId =0;
 function moveBackground(){
@@ -54,3 +52,5 @@ function moveBackground(){
     backgroundImagePositionX = backgroundImagePositionX -20;
     document.getElementById("background").style.backgroundPositionX = backgroundImagePositionX +"px";
 }
+
+
