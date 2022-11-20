40b215391b76e5fcf90600416ed414abac199bd5
AsiriM2000 <asiriambagahawaththa@gmail.com>
2022-11-20 13:39:32 +0530

    update content

--- assignments/js/game/assets/js/Script.js
+++ assignments/js/game/assets/js/Script.js
@@ -1,56 +1,76 @@
-let boy = document.getElementById("boy");
+let girl = document.getElementById("girl");
 
-idleImageNumber = 0;
-idleAnimationNumber = 0;
-flyImageNumber = 0;
-flyAnimationNumber = 0;
+breathingImageNumber = 0;
+breathingAnimationNumber = 0;
 
-function flyAnimation() {
-    flyImageNumber = flyImageNumber + 1;
-    if (flyImageNumber == 11) {
-        flyImageNumber = 1;
+function breathingAnimation() {
+    breathingImageNumber = breathingImageNumber + 1;
+    if (breathingImageNumber == 11) {
+        breathingImageNumber = 1;
     }
-    boy.src = "assets/image/test/Idle%20(1)(" + idleImageNumber + ").png";
+    girl.src = "assets/image/characters/Idle%20(" + breathingImageNumber + ").png";
 }
 
-function flyAnimationStart() {
-    flyAnimationNumber = setInterval(flyAnimation, 100);
-    clearInterval(idleAnimationNumber);
+function breathingAnimationStart() {
+    breathingAnimationNumber = setInterval(breathingAnimation, 200);
 }
 
-function keyCheck(event){
-    var keyCode = event.which;
+runImageNumber =0;
+runAnimationNumber =0;
 
+function runAnimation() {
+    runImageNumber = runImageNumber + 1;
+        if (runImageNumber == 9) {
+            runImageNumber = 1;
+        }
+        girl.src = "assets/image/characters/Run%20(" + runImageNumber + ").png";
+}
 
+function runAnimationStart() {
+    runAnimationNumber = setInterval(runAnimation, 100);
+    clearInterval(breathingAnimationNumber);
 }
 
 function keyCheck(event) {
     var keyCode = event.which;
     if (keyCode == 13) {
-        document.getElementById("gameIntru").style.display = "none";
-        if (moveBackgroundAnimationId == 0) {
-            moveBackgroundAnimationId = setInterval(moveBackground, 100);
+        // document.getElementById("gameIntru").style.display = "none";
+
+        if (runAnimationNumber == 0) {
+            runAnimationStart();
         }
     }
-
-    function idleAnimation() {
-        idleImageNumber = idleImageNumber + 1;
-        if (idleImageNumber == 9) {
-            idleImageNumber = 1;
-        }
-        boy.src = "assets/image/characters/Run%20(" + idleImageNumber + ").png";
-    }
-
-    idleAnimationNumber = setInterval(idleAnimation, 200);
-
-
 }
-var backgroundImagePositionX =0;
-var moveBackgroundAnimationId =0;
-function moveBackground(){
 
-    backgroundImagePositionX = backgroundImagePositionX -20;
-    document.getElementById("background").style.backgroundPositionX = backgroundImagePositionX +"px";
-}
+
+//
+// function keyCheck(event) {
+//     var keyCode = event.which;
+//     if (keyCode == 13) {
+//         document.getElementById("gameIntru").style.display = "none";
+//         if (moveBackgroundAnimationId == 0) {
+//             moveBackgroundAnimationId = setInterval(moveBackground, 100);
+//         }
+//     }
+//
+//     function idleAnimation() {
+//         idleImageNumber = idleImageNumber + 1;
+//         if (idleImageNumber == 9) {
+//             idleImageNumber = 1;
+//         }
+//         boy.src = "assets/image/characters/Run%20(" + idleImageNumber + ").png";
+//     }
+//
+//     idleAnimationNumber = setInterval(idleAnimation, 200);
+//
+//
+// }
+// var backgroundImagePositionX =0;
+// var moveBackgroundAnimationId =0;
+// function moveBackground(){
+//
+//     backgroundImagePositionX = backgroundImagePositionX -20;
+//     document.getElementById("background").style.backgroundPositionX = backgroundImagePositionX +"px";
+// }
 
 
