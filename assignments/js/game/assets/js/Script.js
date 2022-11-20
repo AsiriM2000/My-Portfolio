4e95b39792156b68cfc929b4ac4084a6ad24281b
AsiriM2000 <asiriambagahawaththa@gmail.com>
2022-11-20 14:20:16 +0530

    background animation create

--- assignments/js/game/assets/js/Script.js
+++ assignments/js/game/assets/js/Script.js
@@ -34,43 +34,24 @@ function runAnimationStart() {
 function keyCheck(event) {
     var keyCode = event.which;
     if (keyCode == 13) {
-        // document.getElementById("gameIntru").style.display = "none";
-
+        document.getElementById("gameIntru").style.display = "none";
         if (runAnimationNumber == 0) {
             runAnimationStart();
         }
     }
+
+    if (moveBackgroundAnimationId == 0) {
+        moveBackgroundAnimationId = setInterval(moveBackground, 100);
+    }
 }
 
 
-//
-// function keyCheck(event) {
-//     var keyCode = event.which;
-//     if (keyCode == 13) {
-//         document.getElementById("gameIntru").style.display = "none";
-//         if (moveBackgroundAnimationId == 0) {
-//             moveBackgroundAnimationId = setInterval(moveBackground, 100);
-//         }
-//     }
-//
-//     function idleAnimation() {
-//         idleImageNumber = idleImageNumber + 1;
-//         if (idleImageNumber == 9) {
-//             idleImageNumber = 1;
-//         }
-//         boy.src = "assets/image/characters/Run%20(" + idleImageNumber + ").png";
-//     }
-//
-//     idleAnimationNumber = setInterval(idleAnimation, 200);
-//
-//
-// }
-// var backgroundImagePositionX =0;
-// var moveBackgroundAnimationId =0;
-// function moveBackground(){
-//
-//     backgroundImagePositionX = backgroundImagePositionX -20;
-//     document.getElementById("background").style.backgroundPositionX = backgroundImagePositionX +"px";
-// }
+var backgroundImagePositionX =0;
+var moveBackgroundAnimationId =0;
+function moveBackground(){
+
+    backgroundImagePositionX = backgroundImagePositionX -20;
+    document.getElementById("background").style.backgroundPositionX = backgroundImagePositionX +"px";
+}
 
 
